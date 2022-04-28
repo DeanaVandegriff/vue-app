a4efafe071d9ae121a0242e015bee906b49966a9
deana <dvandegriff1@gmail.com>
2022-04-28 12:32:11 -0500

    adds employee data and create action

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <nav>
     <router-link to="/">Home</router-link>
     |
@@ -27,5 +27,58 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+</style> -->
+
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-form />
+    <employee-table v-bind:employees="employees" />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
+
+export default {
+  name: "app",
+  components: {
+    EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com",
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com",
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
 }
 </style>
